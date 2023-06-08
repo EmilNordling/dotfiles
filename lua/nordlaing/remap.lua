270afcd81f6097a9953a825c07e5647fded1112a
Emil <ekala001@gmail.com>
2023-06-08 15:55:11 +0200

    Adds sick keymap

--- lua/nordlaing/remap.lua
+++ lua/nordlaing/remap.lua
@@ -10,6 +10,7 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set('n', '<cr>', 'ciw')
 
 vim.keymap.set("n", "<leader>vwm", function()
     require("vim-with-me").StartVimWithMe()
