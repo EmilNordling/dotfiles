4faed1847e7dc6cbe344ccba38b60d18a7becb17
Emil Nordling <i.emil.nordling@gmail.com>
2025-02-03 14:28:24 +0100

    Fix cmd arrow in wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -18,6 +18,10 @@ config.keys = {
         mods = 'CMD',
         action = act.ClearScrollback 'ScrollbackAndViewport',
     },
+    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+    { key = "LeftArrow",  mods = "OPT", action = wezterm.action { SendString = "\x1bb" } },
+    -- Make Option-Right equivalent to Alt-f; forward-word
+    { key = "RightArrow", mods = "OPT", action = wezterm.action { SendString = "\x1bf" } },
 }
 config.color_scheme = 'Vacuous 2 (terminal.sexy)'
 
